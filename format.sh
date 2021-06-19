2f5b255af96930f7af479afc11e98573a0b0fe49
xcoder <xgqcoder@sina.com>
2021-06-19 17:53:06 +0800

    update format script

--- format.sh
+++ format.sh
@@ -1,7 +1,6 @@
 buildifier WORKSPACE
 
-find . -not -path './bazel-*' -name BUILD | xargs buildifier
+find . -not -path "./bazel-*" -name BUILD | xargs buildifier
 
-find . -not -path './bazel-*' -name "*.cc" | xargs clang-format -i --sort-includes
-
-find . -not -path './bazel-*' -name "*.h" | xargs clang-format -i --sort-includes
+find . -not -path "./bazel-*" -type f \( -iname "*.cc" -o -iname "*.c" -o -iname "*.cpp" -o -iname "*.h" -o -iname "*.hpp" \) | \
+    xargs clang-format -i --sort-includes
