93992e57edebfb9ab427b60ee7b161d89ae46c34
xcoder <xgqcoder@sina.com>
2022-08-09 11:28:04 +0800

    add a jupyter notebook

---
+++ start_jupyter.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+cd $(dirname "$0")
+
+ps ax | grep -E "jupyter-lab" | grep -v "grep" | awk '{print $1}' | xargs -I {} kill -9 {}
+
+jupyter_log_file='.logs/jupyter.logs'
+
+rm -r ${jupyter_log_file} || True
+
+mkdir -p $(dirname ${jupyter_log_file})
+
+touch ${jupyter_log_file}
+
+nohup jupyter-lab > ${jupyter_log_file} 2>&1 &
+
+echo 'jupyter-lab is running'
+
