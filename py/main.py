b555b51b32d16cb183eefffa56881fbb1ba720e3
xcoder <xgqcoder@sina.com>
2023-08-05 12:08:02 +0800

    update

---
+++ py/main.py
@@ -0,0 +1,7 @@
+class BaseAttention(tf.keras.layers.Layer):
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        self.layernorm = tf.keras.layers.LayerNormalization()
+        self.add = tf.keras.layers.Add()
