d4a540b38d0e1ba27da2540900867844d7e72617
xxxgqcoder <xxxgqcoder@gmail.com>
2025-03-04 17:03:58 +0800

    quick sort

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -1,13 +1,45 @@
 def insert_sort(a):
     for j in range(1, len(a)):
         # assume a[0, ..., j-1] is sorted
-        key = a[j] # make a hole at a[j]
+        key = a[j]  # make a hole at a[j]
         i = j - 1
         while i >= 0 and a[i] > key:
             # initial case: a[j-1] > a[j], move hole one step left
-            a[i+1]  = a[i]
+            a[i + 1] = a[i]
             i = i - 1
         # termination:
         # i >= 0, a[i] < key, i+1 is the place for key (hole)
         # i < 0, all values checked, i = 0 is the place for key (hole)
-        a[i+1] = key
+        a[i + 1] = key
+
+
+def median3(a, left, right):
+    center = (left + right) // 2
+    if a[center] < a[left]:
+        a[left], a[center] = a[center], a[left]
+    if a[right] < a[left]:
+        a[right], a[left] = a[left], a[right]
+    if a[right] < a[center]:
+        a[right], a[center] = a[center], a[right]
+    a[center], a[right - 1] = a[right - 1], a[center]
+    return a[right - 1]
+
+
+def quick_sort(a, left, right):
+    if len(a) <= 1 or left >= right:
+        return
+    pivot = median3(a, left, right)
+    if right - left <= 2:
+        return
+
+    i, j = left + 1, right - 2
+    while i < j:
+        while a[i] < pivot:
+            i += 1
+        while a[j] > pivot:
+            j -= 1
+        if i < j:
+            a[i], a[j] = a[j], a[i]
+    a[i], a[right - 1] = a[right - 1], a[i]
+    quick_sort(a, left, i - 1)
+    quick_sort(a, i + 1, right)
--- algorithms/sort_test.py
+++ algorithms/sort_test.py
@@ -1,26 +1,49 @@
 import unittest
 
-from .sort import insert_sort
+from .sort import insert_sort, quick_sort
+
 
 class SortTest(unittest.TestCase):
+
     def test_sort(self):
         # case 1
         a = [2]
         insert_sort(a)
         self.assertEqual(a, [2])
-    
-        # case 2
-        a = [4,3,2,1]
-        insert_sort(a)
-        self.assertEqual(a, [1,2,3,4])
 
         # case 2
-        a = [4,3,2,1]
+        a = [4, 3, 2, 1]
         insert_sort(a)
-        self.assertEqual(a, [1,2,3,4])
+        self.assertEqual(a, [1, 2, 3, 4])
+
+        # case 2
+        a = [4, 3, 2, 1]
+        insert_sort(a)
+        self.assertEqual(a, [1, 2, 3, 4])
 
 
+class QuickSortTest(unittest.TestCase):
 
+    def test_quick_sort(self):
+        # case 1: short list
+        a = [1]
+        quick_sort(a, 0, len(a) - 1)
+        self.assertEqual(a, [1])
+
+        # case 2: len 3 list
+        a = [4, 1, 7]
+        quick_sort(a, 0, len(a) - 1)
+        self.assertEqual(a, [1, 4, 7])
+
+        # case 3: sorted list
+        a = [8, 7, 5, 4, 2, 1]
+        quick_sort(a, 0, len(a) - 1)
+        self.assertEqual(a, [1, 2, 4, 5, 7, 8])
+
+        # case 4: norm random list
+        a = [5, 4, 2, 6, 7, 8, 9, 3, 1, 4]
+        quick_sort(a, 0, len(a) - 1)
+        self.assertEqual(a, [1, 2, 3, 4, 4, 5, 6, 7, 8, 9])
 
 
 if __name__ == '__main__':
