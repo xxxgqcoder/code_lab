2feed574f58480280bc1dc12d8aad1f3681b4ebe
xxxgqcoder <xxxgqcoder@gmail.com>
2025-03-04 14:23:11 +0800

    binary search

---
+++ algorithms/search.py
@@ -0,0 +1,15 @@
+
+def binary_search(a, x):
+    if len(a) < 1:
+        return -1
+    
+    i, j = 0, len(a) - 1
+    while i <= j:
+        mid = (i + j) // 2
+        if a[mid] == x:
+            return mid
+        elif a[mid] > x:
+            j = mid - 1
+        else:
+            i = mid + 1
+    return -1
---
+++ algorithms/search_test.py
@@ -0,0 +1,34 @@
+import unittest
+
+from .search import binary_search
+
+class SearchTest(unittest.TestCase):
+    def test_search(self):
+        # case 1: 1 elemenet list
+        a = [2]
+        x = 2
+        ret = binary_search(a, x)
+        self.assertEqual(ret, 0)
+
+        # case 2: found
+        a = [0, 1, 2, 3, 4]
+        x = 2
+        ret = binary_search(a, x)
+        self.assertEqual(ret, 2)
+
+        # case 3: exceed right
+        a = [0, 1, 2, 3, 4]
+        x = 5
+        ret = binary_search(a, x)
+        self.assertEqual(ret, -1)
+
+        # case 3: exceed left
+        a = [0, 1, 2, 3, 4]
+        x = -1
+        ret = binary_search(a, x)
+        self.assertEqual(ret, -1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
